641ba7493ee74f8816a63dd98e37de46f06dc209
Samrat Tamrakar <samrattamrakar7@gmail.com>
2025-03-02 21:13:41 +0545

    made footer and assets folder for backend for further use

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
 
 const Footer = () => {
   return (
-<>
-</>
-  )
-}
+    <div className="p-8">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9UXBZMRbRexZzLqeMBm5CtkWzVCGu2vZkOA&s' className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5"> COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600"></ul>
+          <li>+977-090909</li>
+          <li>contact@forever.com</li>
+        </div>
+      </div>
+      <div> 
+        <hr />
+        <p className="py-5 text-sm text-center">Copyright 2024@forever.com - All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
